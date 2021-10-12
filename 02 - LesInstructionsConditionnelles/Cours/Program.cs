7b4b8d6d09a3c8644df0f08cec0ec78407200874
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-10-12 16:35:41 +0200

    Code cours - Instruction Conditionnelle

---
+++ 02 - LesInstructionsConditionnelles/Cours/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTheTruth = false;
+            // On ne rentre pas dans le if parce que le bool est à false
+            if(isTheTruth)
+            {
+                Console.WriteLine(isTheTruth);
+            }
+            
+            isTheTruth = true;
+            if(isTheTruth)
+            {
+                Console.WriteLine(isTheTruth);
+            }
+
+            if(!isTheTruth)
+            {
+                Console.WriteLine(isTheTruth);
+            }
+
+            // Operateur de comparaison
+            // Besoin d'avoir u le même type de chaque coté de l'operateur de comparaison
+            // ou un type compatible ex : float et int
+
+            int myValue = 1;
+            float myValueFloat = 1.0f;
+            string name = "roger";
+            if(myValue == myValueFloat)
+            {
+                Console.WriteLine(myValue);
+            }
+
+            if(!(myValue == myValueFloat))
+            {
+                Console.WriteLine(myValue);
+            }
+
+            int value1 = 10;
+            int value2 = 23;
+
+            bool testCompareOperator = value1 >= value2;
+            testCompareOperator = value2 != value1;
+
+            bool testCompareOperator2 = value1 < value2;
+
+            testCompareOperator = !testCompareOperator2;
+
+            // Operateur de logique || , && et !
+            if(testCompareOperator || testCompareOperator2)
+            {
+                Console.WriteLine(testCompareOperator);
+            }
+
+            if(value1 >= value2 || value2 != value1 && value1 < value2)
+            {
+                Console.WriteLine(testCompareOperator);
+            }
+
+            int x = 1;
+            if(x >= 0 && x <= 10)
+            {
+                Console.WriteLine(x);
+            }
+
+            bool testCompareOperator3 = value1 >= value2 || value2 != value1 && value1 < value2;
+
+            // Si x entre 0 et 10
+            if(x >= 0 && x <= 10)
+            {
+                Console.WriteLine(x);
+            }
+            else if(x > 10 && x <= 40) // Si x entre 11 et 40
+            {
+
+            }
+            else if(x > 40) // si x superieur à 40
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Les nombres négatifs ");
+            }
+        }
+    }
+}
