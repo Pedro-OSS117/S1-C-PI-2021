87674704482f7dc554ed5b692f633577821c612b
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-10-26 14:27:01 +0200

    Fin correction cours fonctions

---
+++ 03 - LesFonctions/AreYouMajor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AreYouMajor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Quelle est votre année de naissance ?");
+            int dateDonnee = int.Parse(Console.ReadLine());
+            
+            if(DetermineMajorite(CalculeAge(dateDonnee)))
+            {
+                Console.WriteLine("Vous êtes majeur !");
+            }
+            else
+            {                
+                Console.WriteLine("Vous n'êtes pas majeur !");
+            }
+        }
+
+        static int CalculeAge(int naisssanceDate)
+        {
+            return DateTime.Today.Year - naisssanceDate;
+        }
+
+        static bool DetermineMajorite(int age)
+        {
+            return age >= 18;
+        }
+    }
+}
