3610ab2b7a691f1753921b0f91721db3bba79e03
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-11-09 13:29:45 +0100

    Correction Helper Read et Exo Boucles

---
+++ 04 - LesBoucles/Exos/ExosBoucles/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ExosBoucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------ DisplayAllNumber
+            Console.WriteLine( "------------ DisplayAllNumber");
+            DisplayAllNumber(4);
+            DisplayAllNumber(7);
+            DisplayAllNumber(75);
+            
+            // ------------ DisplayAllCharacter
+            Console.WriteLine( "\n------------ DisplayAllCharacter");
+            DisplayAllCharacter(3);
+            DisplayAllCharacter(18);
+            DisplayAllCharacter(34);
+
+            // ------------ DisplayAllPairNumber
+            Console.WriteLine( "\n------------ DisplayAllPairNumber");
+            DisplayAllPairNumber(11);
+            DisplayAllPairNumber(28);
+            DisplayAllPairNumber(63);
+
+            // ------------ DisplayTriangle
+            Console.WriteLine( "\n------------ DisplayTriangle");
+            DisplayTriangle(4);
+            DisplayTriangle(8);
+            DisplayTriangle(11);
+
+            // ------------ DisplayDiamond
+            Console.WriteLine( "\n------------ DisplayDiamond");
+            DisplayDiamond(2);
+            DisplayDiamond(4);
+            DisplayDiamond(5);
+            DisplayDiamond(11);
+        }
+
+        static void DisplayAllNumber(int maxNumber)
+        {
+            string toDisplay = "";
+            for(int i = 0; i <= maxNumber; i++)
+            {
+                // Concatenation
+                toDisplay += i;
+            }
+            Console.WriteLine(toDisplay);
+        }
+
+        static void DisplayAllCharacter(int maxNumber)
+        {
+            int startIndex = 33;
+            maxNumber += startIndex;
+            for(int i = startIndex; i <= maxNumber; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+
+        static void DisplayAllPairNumber(int maxNumber)
+        {
+            string toDisplay = "";
+            for(int i = 0; i <= maxNumber; i +=2)
+            {
+                toDisplay += i.ToString();
+            }
+            Console.WriteLine(toDisplay);
+        }
+
+        static void DisplayTriangle(int maxNumber)
+        {
+            /*for(int i = 0; i <= maxNumber; i ++)
+            {
+                string toDisplay = "";
+                for(int j = 0; j < i; j++)
+                {
+                    toDisplay += '*';
+                }
+                Console.WriteLine(toDisplay);
+            }*/
+
+            // Good
+            string message = "";
+            for(int i = 0; i <= maxNumber; i++)
+            {
+                message += "*";                
+                Console.WriteLine(message);
+            }
+        }
+
+        static void DisplayDiamond(int maxNumber)
+        {
+            for(int i = 1; i <= maxNumber; i ++)
+            {
+                DisplayLineDiamond(maxNumber, i);
+            }
+
+            for(int i = maxNumber - 1; i >= 1; i--)
+            {
+                DisplayLineDiamond(maxNumber, i);
+            }
+        }
+
+        static void DisplayLineDiamond(int maxNumber, int line)
+        {
+            string toDisplay = "";
+            for(int j = 0; j < maxNumber - line; j++)
+            {
+                toDisplay += ' ';
+            }
+
+            for(int j = maxNumber - line; j < line + maxNumber - 1; j++)
+            {
+                toDisplay += '*';
+            }
+
+            // Facultatif
+            for(int j = maxNumber + 1; j < line + maxNumber; j++)
+            {
+                toDisplay += ' ';
+            }
+            Console.WriteLine(toDisplay);
+        }
+    }
+}
--- 04 - LesBoucles/HelperRead/Program.cs
+++ 04 - LesBoucles/HelperRead/Program.cs
@@ -6,10 +6,12 @@ namespace Helper
     {
         static void Main(string[] args)
         {
-            int valueUser = HelperRead.ReadInt();
+            /*int valueUser = HelperRead.ReadInt();
             Console.WriteLine("La valeur entrée par l'utilisateur est : " + valueUser);
             valueUser = HelperRead.ReadInt("Bonjour mon petit ami, si tu rentres une valeur, tu auras une sucette : ");
-            Console.WriteLine("La valeur entrée par l'utilisateur est : " + valueUser);
+            Console.WriteLine("La valeur entrée par l'utilisateur est : " + valueUser);*/
+
+            HelperRead.ReadIntMinMax(1, 4);
         }
     }
 }
