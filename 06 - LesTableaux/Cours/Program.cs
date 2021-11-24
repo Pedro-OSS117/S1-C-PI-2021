12841b3afdf4f5b686726d18fbca58fccae1ff4a
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-11-24 13:04:48 +0100

    Cours et DM

---
+++ 06 - LesTableaux/Cours/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration
+            char[] monMot = null;
+
+            // Initialisation
+            int[] mesValeurs = new int[4];
+            monMot = new char[5];
+            Console.WriteLine("Tableau monMot : " + monMot.Length);
+            Console.WriteLine("Tableau mesValeurs : " + mesValeurs.Length);
+
+            // Accès éléments du tableau
+            Console.WriteLine(mesValeurs[0]);
+            Console.WriteLine(mesValeurs[1]);
+            Console.WriteLine(mesValeurs[3]);
+            Console.WriteLine(mesValeurs[mesValeurs.Length - 1]);
+            Console.WriteLine(monMot[0]);
+
+            // Affectation d'une valeur d'un tableau
+            mesValeurs[1] = 4;
+            Console.WriteLine(mesValeurs[1]);
+
+            monMot[3] = 'z';
+            Console.WriteLine(monMot[3]);
+
+            // Accès Interdi à l'exterieur du tableau
+            // mesValeurs[7] = 4;
+            // mesValeurs[-1] = 4;
+            
+            // Declaration avec assignation de valeurs par défaut directement.
+            int[] monTableau1 = {1, 2, 3};
+            char[] monTableauDeChar = {'1', '2', (char)3};
+
+        }
+    }
+}
---
+++ 06 - LesTableaux/ExosCours/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ExosCours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] monTableau = new int[4];
+            monTableau[0] = 1;
+            monTableau[1] = 9;
+            monTableau[2] = 4;
+            monTableau[3] = 8;*/
+            // Exo 1 - Display Tab
+            int[] monTableau = {1 , 9, 4, 8};
+            DisplayTab(monTableau);
+            Console.WriteLine("");
+
+            DisplayTabAvecClasse(monTableau);
+            char[] monTableauDeLettre = {'b', 'o', 'b', 'y'};
+
+            DisplayTabAvecClasseGenerique<char>(monTableauDeLettre);
+            Console.WriteLine("");
+
+            // Exo 2 - Total tab
+            int total = TotalTab(monTableau);
+            
+            Console.Write("Le total du tableau de valeurs : ");
+            DisplayTabAvecClasse(monTableau);
+            Console.WriteLine(" est de : " + total);
+            
+            // Exo 2 Bis - Moyenne tab
+            float moyenne = MoyenneTab(monTableau);
+            Console.Write("La moyenne du tableau de valeurs : ");
+            DisplayTabAvecClasse(monTableau);
+            Console.WriteLine(" est de : " + moyenne);
+        }
+        
+        // Exo 2 - Total tab
+        static int TotalTab(int[] myTab)
+        {
+            int result = 0;
+            for(int i = 0; i < myTab.Length; i++)
+            {
+                result += myTab[i];
+            }
+            return result;
+        }
+
+        // Exo 2 Bis - Moyenne tab
+        static float MoyenneTab(int[] myTab)
+        {
+            float total = TotalTab(myTab);
+            return total / myTab.Length;
+        }
+
+        static void DisplayTab(int[] mesValeurs)
+        {
+            for(int i = 0; i < mesValeurs.Length; i++)
+            {
+                Console.WriteLine(mesValeurs[i]);
+            }
+
+            return;
+        }
+
+        static void DisplayTabAvecClasse(int[] mesValeurs)
+        {
+            string display = "{";
+            for(int i = 0; i < mesValeurs.Length - 1; i++)
+            {
+                display += "\"" + mesValeurs[i] + "\",";
+            }
+            display += "\"" + mesValeurs[mesValeurs.Length - 1] + "\"}";
+            Console.Write(display);
+        }
+
+        static void DisplayTabAvecClasseGenerique<T>(T[] myTab)
+        {
+            if(myTab != null)
+            {
+                string display = "{";
+                for(int i = 0; i < myTab.Length - 1; i++)
+                {
+                    if(myTab[i] != null)
+                    {
+                        display += "\"" + myTab[i].ToString() + "\",";
+                    }
+                }
+                if(myTab[myTab.Length - 1] != null)
+                {
+                    display += "\"" + myTab[myTab.Length - 1].ToString() + "\"}";
+                }
+                Console.Write(display);
+            }
+        }
+    }
+}
